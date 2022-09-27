ca0bf7ef8ca4cf0e82bb1b805622cb52b1ec3c63
JP271 <95508297+JP271@users.noreply.github.com>
2022-09-27 09:18:03 -0600

    Update Rectangle.java

--- shapes/Rectangle.java
+++ shapes/Rectangle.java
@@ -11,23 +11,41 @@ public class Rectangle{
 
     
     public Rectangle(double len){
+        if(len <= 0){
+      throw new IllegalArgumentException("The length of a rectangle must be positive");
+    }
+        
         l = len;
         w = len;
     }
 
     
     public Rectangle(double len, double wid){
+        if(len <= 0){
+      throw new IllegalArgumentException("The length of a rectangle must be positive");
+    } else if(wid <=0){
+      throw new IllegalArgumentException("The width of a rectangle must be positive");
+        }
+        
         l = len;
         w = wid;
     }
 
     
     public void setLength(double len){
+        if(len <= 0){
+      throw new IllegalArgumentException("The length of a rectangle must be positive");
+    }
+        
         l = len;
     }
 
     
     public void setWidth(double wid){
+        if(len <= 0){
+      throw new IllegalArgumentException("The width of a rectangle must be positive");
+    }
+        
         w = wid;
     }
 
