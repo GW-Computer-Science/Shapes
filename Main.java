5634c02343a9f2e714e1af93f16a90d5bd0cccb7
gmoney4541 <71167145+gmoney4541@users.noreply.github.com>
2022-09-26 20:46:20 -0600

    Update Main.java
    Cleared the Main.

--- Main.java
+++ Main.java
@@ -2,11 +2,6 @@ import shapes.*;
 
 class Main {
   public static void main(String[] args) {
-    Circle c1 = new Circle();
-    Circle c2 = new Circle();
-
-    RegularPolygon p1 = new RegularPolygon(4, 2.0);
-      
-    System.out.println(p1.getArea());
+    
   }
 }
